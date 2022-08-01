7cfc0451fbb1547e6d4898a981499d69738dfa01
zhaojianwei02 <zhaojianwei02@zuoyebang.com>
2022-08-01 15:10:40 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # UseKoin
 Koin（依赖注入）-使用
 
+### 基本概念
+- 懒加载 与 立即加载
+// Lazy Inject ViewModel，注入依赖
+val mainViewModel: MainViewModel by viewModel()
+// Inject ViewModel, not lazy
+//    val mainViewModel: MainViewModel = getViewModel() //可能会报错：Your activity is not yet attached to the Application instance. You can't request ViewModel before onCreate call
+
+    // Lazy Inject Class
+    val person: Person by inject()
+    // Inject Class, not lazy
+//    val person: Person = get()
+
 ### 基本语法
 
 - startKoin
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.getViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     // Lazy Inject ViewModel，注入依赖
     val mainViewModel: MainViewModel by viewModel()
+    // Inject ViewModel, not lazy
+//    val mainViewModel: MainViewModel = getViewModel() //可能会报错：Your activity is not yet attached to the Application instance. You can't request ViewModel before onCreate call
 
     // Lazy Inject Class
-    val person: Person = get()
+    val person: Person by inject()
+    // Inject Class, not lazy
+//    val person: Person = get()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
